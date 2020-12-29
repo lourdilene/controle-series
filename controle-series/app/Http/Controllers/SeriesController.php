928d787ee9b2371f81f5dd7611f489e2e9f0383a
Lourdilene Souza <lourdilene.souza@gmail.com>
2020-12-29 10:08:57 -0300

    Teste de parametros na url

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class SeriesController extends Controller
 {
-    public function listarSeries(){
+    public function listarSeries(Request $request){
+
+        echo $request->query('parametro');
+        exit();
+
         $series = ['Friends', 
         'Doutor House', 'Os pinguins de Madagascar'];
 
