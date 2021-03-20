494e1212916de2d323679aa4cd49c285b95ce179
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-03-20 15:59:23 -0300

    Salvar série no banco e buscar séries 04.MVC-model.06-Eloquent ORM

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -5,7 +5,7 @@ Séries
 @endsection
 
 @section('conteudo')
-<a href="series/criar" class="btn btn-dark mb-2">Adicionar</a>
+<a href="series/criar" class="btn btn-dark mb-2">Adicionar Série</a>
   <ul class="list-group">
           @foreach ($series as $serie)
            <li class="list-group-item">{{$serie->nome}}</li>          
