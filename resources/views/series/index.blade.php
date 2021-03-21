62f9dbc4e2785ffc5c3014cc7b7a3c304b22d1c8
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-03-21 20:39:50 -0300

    Exclusão de item da lista. 05-Lapidando a aplicação. 06-Excluindo uma série

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -15,7 +15,14 @@ Séries
 <a href="series/criar" class="btn btn-dark mb-2">Adicionar Série</a>
   <ul class="list-group">
           @foreach ($series as $serie)
-           <li class="list-group-item">{{$serie->nome}}</li>          
+           <li class="list-group-item">{{$serie->nome}}
+              <form method="POST" action="/series/{{$serie->id}}"
+                onsubmit="return confirm('Tem certeza que deseja excluir ?')">
+                  @csrf
+                  @method('DELETE')
+                <button class="btn btn-danger">Excluir</button>
+              </form>
+          </li>          
           @endforeach               
   </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Route::get('/series', 'SeriesController@index');
 Route::get('/series/criar', 'SeriesController@create');
 Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
