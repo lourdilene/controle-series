01bfe325d5bff69ce93f9c1b60ee1d08c819cf5e
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-05-18 23:24:37 -0300

    criação de edição com javascript

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -16,9 +16,23 @@ Séries
   <ul class="list-group">
           @foreach ($series as $serie)
            <li class="list-group-item d-flex justify-content-between align-items-center">
-             {{$serie->nome}}
+             
+            <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+
+            <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+                <input type="text" class="form-control" value="{{ $serie->nome }}">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+                        <i class="fas fa-check"></i>
+                    </button>
+                    @csrf
+                </div>
+            </div>
 
             <span class="d-flex">
+              <button class="btn btn-info btn-sm" onclick="toggleInput({{$serie->id}})">
+                <i class="fas fa-edit">Editar</i>
+              </button>
               <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-1">
                 <i class="fas fas-external-link-alt">Visualizar</i>
               </a>
@@ -33,4 +47,18 @@ Séries
           </li>          
           @endforeach               
   </ul>
+  <script>
+    function toggleInput(serieId){            
+      const nomeSerieEl = document.getElementById(`nome-serie-${serieId}`);
+      const inputSerieEl = document.getElementById(`input-nome-serie-${serieId}`);
+      if (nomeSerieEl.hasAttribute('hidden')){
+        nomeSerieEl.removeAttribute('hidden');
+        inputSerieEl.hidden = true;
+      }else{
+
+        inputSerieEl.removeAttribute('hidden');
+        nomeSerieEl.hidden = true;
+      }
+    }
+  </script>
 @endsection
